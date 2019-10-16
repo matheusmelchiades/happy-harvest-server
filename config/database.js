f464448067b88dff72f8983b0e694be5ec6b822b
Matheus Melchiades <matheusmacielmelchiades@hotmail.com>
2019-10-16 15:23:15 -0300

    config circle env

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 require('dotenv').config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+    path: process.env.NODE_ENV === 'test' ? `${process.cwd()}/.env.test` : `${process.cwd()}/.env`
 });
 
 global.database = {};
 
 module.exports = {
-    host: process.env.DB_HOSTNAME || 'localhost',
-    username: process.env.DB_USERNAME || 'admin',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'db',
+    host: process.env.DB_HOSTNAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     dialect: process.env.DB_DIALECT || 'postgres',
     storage: `${process.cwd()}/__tests__/database.sqlite`,
     logging: process.env.NODE_ENV === 'development',
