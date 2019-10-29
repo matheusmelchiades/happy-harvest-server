6409c7d19bb4663b01858e9acf9eb3b7125ed4f9
Matheus Melchiades <matheusmacielmelchiades@hotmail.com>
2019-10-29 18:03:46 -0300

    create factory listing

---
+++ app/api/components/factory.js
@@ -0,0 +1,16 @@
+function buildHeaders(data) {
+    const [firstItem] = data.rows;
+
+    return Object.keys(firstItem.dataValues).map(name => {
+        const attibutes = firstItem.rawAttributes;
+        const type = attibutes[name].type.constructor.name.toLowerCase();
+
+        return { name, type };
+    });
+}
+
+module.exports.listing = data => ({
+    count: data.count,
+    headers: buildHeaders(data),
+    rows: data.rows
+});
